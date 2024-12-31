b29fe1bda26e5f7a3f426ae6d2165534744ea81e
twogarlic <hanle0126@naver.com>
2024-12-31 23:07:06 +0900

    [UI] homefragment 제작

---
+++ app/src/main/java/com/roadjourney/Home/GoalItem.kt
@@ -0,0 +1,10 @@
+package com.roadjourney.Home
+
+data class GoalItem(
+    val goal: String,
+    val detail: String,
+    val dDay: String,
+    val progressText: String,
+    val progressValue: Int,
+    val priority: Int
+)
