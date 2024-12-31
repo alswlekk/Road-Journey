883b34178939409792ebf6ee0ef7e40e90ea1452
twogarlic <hanle0126@naver.com>
2025-01-01 00:57:34 +0900

    [UI] mypage, profile

---
+++ app/src/main/java/com/roadjourney/MyPage/ProfileActivity.kt
@@ -0,0 +1,17 @@
+package com.roadjourney.MyPage
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.roadjourney.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
